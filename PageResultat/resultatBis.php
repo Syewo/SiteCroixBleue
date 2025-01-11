d6cc5459574b325e34279b5d4edb9df54ead74c9
Adam ZIANE <adam.raphael.ziane@gmail.com>
2025-01-11 19:38:14 +0100

    utilisation d3.js

--- PageResultat/resultatBis.php
+++ PageResultat/resultatBis.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Résultats de l'Enquête</title>
-    <script src="https://d3js.org/d3.v7.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <link rel="stylesheet" href="resultatBis.css">
 </head>
 <body>
